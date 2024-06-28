4ab9dcf6a157871147d67e23de1d8cf96fb37352
Richa Parikh <richa.parikh@mobilelive.ca>
2024-06-28 07:48:17 -0400

    refactored middleware

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var app = express();
+var middleware = require('./middleware.js')
 
 var PORT = 3000;
-var middleware = {
-    requireAuthentication: function(req, res, next) {
-        console.log('Private route hit!');
-        next()
-    },
-    logger: function(req,res, next){
-        console.log('Request: '+ new Date().toString() +' '+req.method+' '+req.originalUrl);
-        next();
-    }
-}
+
 
 app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function(req,res){
